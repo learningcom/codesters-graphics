0d71b0c0059ab47f662f5a0c762ec9f1bdaf8af8
Natan <natan@codesters.com>
2015-07-02 14:07:30 -0400

    Examples

---
+++ examples/example.py
@@ -0,0 +1,17 @@
+import codesters
+import time
+
+#Making a stage
+stage = codesters.Stage()
+
+#Making a circle
+circle = codesters.Sprite()
+circle.set_x(100)
+circle.set_y(100)
+circle.go_to(0,0)
+circle.set_speed(1)
+
+circle.glide_to(100,100)
+
+#Workaround; for right now, this like has to be in the program being run, rather than __init__.py as it should.
+codesters.root.mainloop()
