cee096b6c2e5671c58de096b807d03d506e5930b
Natan <natan@codesters.com>
2015-07-13 14:59:50 -0400

    Natan trying to get accustomed to this code, trying to fix jumpiness.

--- codesters/Sprite.py
+++ codesters/Sprite.py
@@ -28,6 +28,8 @@ class SpriteClass:
         self.Elements = Elements
         self.animation_duration=1000
         self.speed = 1
+        self.xcor = canvas.winfo_reqwidth()/2
+        self.ycor = canvas.winfo_reqheight()/2
         if image != '':
             self.photo = Image.open("./codesters/sprites/"+image+".gif")
 
@@ -35,7 +37,7 @@ class SpriteClass:
     def draw(self):
         if self.photo != None and self.hidden == False:
             self.bg_photoimg = ImageTk.PhotoImage(self.photo)
-            self.canvas.create_image((self.xcor,self.ycor), image = self.bg_photoimg)
+            self.canvas.create_image((self.xcor + self.canvas.winfo_reqwidth()/2, self.canvas.winfo_reqheight()/2 - self.ycor), image = self.bg_photoimg)
         elif self.hidden == False:
             self.canvas.create_oval((self.xcor-(self.size/2),self.ycor-(self.size/2),self.xcor+(self.size/2),self.ycor+(self.size/2)), fill=self.color)
 
@@ -45,9 +47,11 @@ class SpriteClass:
         self.hidden=False
     #Set variables
     def set_x(self, newx):
-        self.xcor = newx+self.canvas.winfo_width()/2
+        self.xcor = newx
+        self.future_x = self.xcor
     def set_y(self, newy):
-        self.ycor = self.canvas.winfo_height()/2-newy
+        self.ycor = newy
+        self.future_y = self.ycor
     def get_x(self):
         return self.xcor
     def get_y(self):
@@ -69,25 +73,14 @@ class SpriteClass:
         self.heading = degrees * math.pi * 2 / 360
 
     #Basic motion
-    #Step functions to build other functions
-    def step_forward(self):
-        self.set_x(self.xcor + self.speed*math.cos(self.heading))
-        self.set_y(self.ycor + self.speed*math.sin(self.heading))
-    def step_backward(self):
-        self.set_x(self.xcor - self.speed*math.cos(self.heading))
-        self.set_y(self.ycor - self.speed*math.sin(self.heading))
     def move_forward(self, amount):
-        dist = amount
-        while dist > 0:
-            self.step_forward()
-            dist -= self.speed
-            time.sleep(0.01)
+        xdist = amount * math.cos(self.heading)
+        ydist = amount * math.sin(self.heading)
+        self.glide_to(self.future_x + xdist, self.future_y + ydist)
     def move_backward(self, amount):
-        dist = amount
-        while dist > 0:
-            self.step_backward()
-            dist -= self.speed
-            time.sleep(0.01)
+        xdist = amount * math.cos(self.heading)
+        ydist = amount * math.sin(self.heading)
+        self.glide_to(self.future_x - xdist, self.future_y - ydist)
     def move_left(self, amount):
         self.glide_to(self.future_x-amount,self.future_y)
     def move_right(self, amount):
@@ -99,27 +92,37 @@ class SpriteClass:
 
     #More complex motion
     def go_to(self, newx, newy):
-        self.set_x(newx)
-        self.set_y(newy)
+        self.animation_x_coords.append(newx)
+        self.animation_y_coords.append(newy)
+        self.xcor = newx
+        self.ycor = newy
+        self.future_x = newx
+        self.future_y = newy
 
     def goto(self, newx, newy):
-        self.set_x(newx)
-        self.set_y(newy)
+        self.animation_x_coords.append(newx)
+        self.animation_y_coords.append(newy)
+        self.xcor = newx
+        self.ycor = newy
+        self.future_x = newx
+        self.future_y = newy
 
     def glide_to(self, newx, newy):
+        print self.future_x, " ", self.future_y
         xdist = float(newx - self.future_x)
         ydist = float(newy - self.future_y)
-        #dist = math.sqrt(xdist**2 + ydist**2)
+        dist = math.sqrt(xdist**2 + ydist**2)
+        self.animation_duration = dist/self.speed
         frames_needed = (self.animation_duration / 22)
         x_step_size = xdist/frames_needed
         y_step_size = ydist/frames_needed
         for n in range(int(frames_needed)):
             self.animation_x_coords.append(self.future_x+(x_step_size+(x_step_size * n)))
             self.animation_y_coords.append(self.future_y+(y_step_size+(y_step_size * n)))
-        self.future_x = newx
-        self.future_y = newy
-        print self.future_x, " ", self.future_y
-        print self.animation_x_coords, self.animation_y_coords
+        self.future_x = self.animation_x_coords[-1]
+        self.future_y = self.animation_y_coords[-1]
+        #print self.future_x, " ", self.future_y
+        #print self.animation_x_coords, self.animation_y_coords
 
         # tempheading = self.heading
         # self.heading = math.atan(ydist/xdist)
