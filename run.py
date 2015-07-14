fbbb089b61c8f3bf0e5654da455e909cfd865e55
Thomas Ricard <thomas@codesters.com>
2015-07-14 14:38:47 -0400

    renamed class files

--- run.py
+++ run.py
@@ -23,20 +23,6 @@ class App(object):
 
     def do(self):
         execfile(filename)
-        # stage = codesters.Environment()
-        # stage.set_background("summer")
-        # sprite = codesters.Sprite("Alien1")
-        # sprite.set_speed(.5)
-        # sprite.move_up(100)
-        # sprite.turn_left(90)
-        # sprite.wait(2)
-        # sprite.glide_to(0,0)
-        # sprite.wait(3)
-        # sprite.right(70)
-
-        #def moveToMouse(event):
-        #    sprite.glide_to(event.x-250, (event.y-250)*-1)
-        #stage.event_click(moveToMouse)
 
     def move_one(self):
         self.manager.run()
