5bbe4f180394b455c2ab12874c5448d4d92ccb7d
Thomas Ricard <ricard.thomasj@gmail.com>
2015-08-27 13:51:25 -0400

    continued tests

--- tests/test_codesters.py
+++ tests/test_codesters.py
@@ -10,3 +10,13 @@ def test_go_to():
     x = coords[0]
     y = coords[1]
     assert x == 200 and y == 300
+
+def right(amount):
+    sprite = codesters.Sprite()
+    sprite.move_right(amount)
+    return sprite.get_x()
+
+def test_right():
+    amount_to_move_right = 79
+    new_x = right(amount_to_move_right)
+    assert amount_to_move_right == new_x
