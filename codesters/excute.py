41f742874e91b00302bac285bd265045e0044d27
Thomas Ricard <ricard.thomasj@gmail.com>
2015-08-27 13:02:47 -0400

    removed test dir

--- codesters/excute.py
+++
@@ -1,8 +0,0 @@
-import run
-
-if len(sys.argv) != 2:
-        print 'You must supply a filename to run with the codesters library e.g. "python run.py example1.py"'
-        exit()
-filename = sys.argv[1]
-
-run.run(filename)
