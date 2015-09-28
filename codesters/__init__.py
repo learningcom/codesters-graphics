6bc8659a942c42d2c5af0559052372cb7dea6fb5
Thomas Ricard <thomas@codesters.com>
2015-09-28 17:36:42 -0400

    renamed some example projects

--- codesters/__init__.py
+++ codesters/__init__.py
@@ -7,8 +7,8 @@ import sys
 import os
 
 # This can't be the best way to do this
-# We want to skip this part when we use the command line tool (e.g. codesters --example flappydino.py)
-# We want to run this part when we just use python to run the file (e.g. python flappydino.py)
+# We want to skip this part when we use the command line tool (e.g. codesters --example flappyfox.py)
+# We want to run this part when we just use python to run the file (e.g. python flappyfox.py)
 # when we just use python, the first arg is the python file, thus:
 if sys.argv[0][-3:] == '.py':  # does the arg end in .py?
     loc = os.path.join(os.getcwd(), sys.argv[0])
